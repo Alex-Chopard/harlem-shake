e1b442bc7562205a6db538c9b7466edd77f75bf5
Alex Chopard <alex.chopard1998@gmail.com>
2018-12-28 23:18:15 +0100

    Now it works

--- index.js
+++ index.js
@@ -1,110 +1,123 @@
+"use strict";
 
-module.exports =  async function harlemShake () {
-	return new Promise(resolve => {
-		addAndPlayMusic()
-		await animSolo()
-	
-		for (let i = 0; i < 18; i++) {
-			setTimeout(() => {
-				bordel()
-			}, i * 800)
-	
-			if(i === 17) {
-				setTimeout(() => {
-					location.reload()
-					resolve(true)
-				}, (i + 1) * 800)
-			}
-		}
-	})
-}
 
-function addAndPlayMusic () {
-	const body = document.querySelector('body')
-	if (body) {
-		const audio = document.createElement('audio')
-		audio.src = './assets/harlem-shake.mp3.mp3'
-		audio.autoplay = true
-
-		body.append(audio)
+class HarlemShake {
+	constructor ({ singleDancer = 'body', allDancer = ['p', 'img', 'a', 'label'] }) {
+		this.singleDancer = singleDancer;
+		this.allDancer = allDancer;
 	}
-}
 
-async function animSolo(selector) {
-	return new Promise(resolve => {
-		const animations = [
-			'pulse',
-			'flash',
-			'shake',
-			'swing',
-			'tada',
-			'wobble',
-			'jello',
-			'bounce',
-			'flip',
-			'rollOut',
-			'rollIn',
-			'zoomIn',
-			'zoomOut',
-			'hinge',
-			'slideOutUp',
-			'slideInUp',
-			'rotateOut'
-		]
+	start () {
+		return new Promise(async resolve => {
+			this.addAndPlayMusic();
 	
-		const element = document.querySelector(selector)
+			// Start to play the part with the single 'dancer'.
+			await this.singleAnimation();
+		
+			for (let i = 0; i < 18; i++) {
+				await sleep(800);
+				this.bordel();
+			}
 	
-		if (element) {
-			await Promise.all(animations.map(async animation => {
-				element.classList.add('animated')
-				element.classList.add(animation)
+			// Reload the web page for avoid all changes.
+			location.reload();
+			resolve(true);
+		});
+	}
+
+	addAndPlayMusic () {
+		const body = document.querySelector('body');
 	
-				await sleep(1000)
+		if (body) {
+			const audio = document.createElement('audio');
+			audio.src = '../assets/harlem-shake.mp3';
+			audio.autoplay = true;
 	
-				element.classList.remove('animated')
-				element.classList.remove(animation)
-			}))
+			body.append(audio);
 		}
+	}
 
-		resolve(true)
-	})
-}
+	singleAnimation () {
+		return new Promise(async resolve => {
+			const animations = [
+				{ name: 'pulse', time: 1000 },
+				{ name: 'flash', time: 1000 },
+				{ name: 'shake', time: 1000 },
+				{ name: 'swing', time: 1000 },
+				{ name: 'tada', time: 1000 },
+				{ name: 'wobble', time: 1000 },
+				{ name: 'jello', time: 1000 },
+				{ name: 'bounce', time: 1500 },
+				{ name: 'flip', time: 1500 },
+				{ name: 'rollOut', time: 2000 },
+				{ name: 'rollIn', time: 1000 },
+				{ name: 'zoomIn', time: 1000 },
+				{ name: 'zoomOut', time: 1000 },
+				{ name: 'hinge', time: 1000 },
+				{ name: 'slideOutUp', time: 1000 },
+				{ name: 'slideInUp', time: 1000 },
+				{ name: 'rotateOut', time: 1000 }
+			];
+		
+			const element = document.querySelector(this.singleDancer);
+	
+			if (element) {
+				for (var i = 0; i < animations.length; i++) {
+					const animation = animations[i];
+	
+					element.classList.add('animated');
+					element.classList.add(animation.name);
+		
+					await sleep(animation.time);
+		
+					element.classList.remove('animated');
+					element.classList.remove(animation.name);
+				}
+			}
+	
+			resolve(true);
+		})
+	}
 
-function bordel() {
-	const arrayElement = document.querySelectorAll('p, img, a, label')
+	bordel() {
+		const arrayElement = document.querySelectorAll(this.allDancer.join());
+	
+		const arrayAnimations = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing'
+		, 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'fadeIn', 'fadeInDown',
+		, 'fadeInDownBig', 'fadeInLeft', 'flip', 'flipInX', 'flipInY', 'lightSpeedOut', 'rotateIn', 'rotateInDownLeft', 
+		'rotateInDownRight', 'slideOutLeft', 'slideOutRight', 'zoomInUp',
+		'zoomOut', 'zoomOutDown', 'hinge', 'rollIn'];
+	
+		arrayElement.forEach(element => {
+			const indiceRandomAnimation = Math.floor(((Math.random() * 10000 ) % arrayAnimations.length));
+			element.classList.add('animated');
+			element.classList.add(arrayAnimations[indiceRandomAnimation]);
 
-	const arrayAnimations = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing'
-	, 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'fadeIn', 'fadeInDown',
-	, 'fadeInDownBig', 'fadeInLeft', 'flip', 'flipInX', 'flipInY', 'lightSpeedOut', 'rotateIn', 'rotateInDownLeft', 
-	'rotateInDownRight', 'slideOutLeft', 'slideOutRight', 'zoomInUp', 
-	'zoomOut', 'zoomOutDown', 'hinge', 'rollIn']
-
-	arrayElement.forEach(element => {
-		const indiceRandomAnimation = Math.floor(((Math.random() * 10000 ) % arrayAnimations.length));
-		element.classList.add('animated')
-		element.classList.add(arrayAnimations[indiceRandomAnimation])
-		// Set random color
-		element.style.color = randomColor()
-		element.style.backgroundColor = randomColor()
-
-		setTimeout(() => {
-			// Remove class
-			element.classList.remove('animated')
-			element.classList.remove(arrayAnimations[indiceRandomAnimation])
-		}, 700)
-	})
+			// Set random color
+			element.style.color = randomColor();
+			element.style.backgroundColor = randomColor();
+	
+			setTimeout(() => {
+				// Remove class
+				element.classList.remove('animated');
+				element.classList.remove(arrayAnimations[indiceRandomAnimation]);
+			}, 700);
+		})
+	}
 }
 
 /***** UTILS *****/
 
 function sleep (time) {
-	return new Promise(resolve => { setTimeout(resolve, time) })
+	return new Promise(resolve => { setTimeout(resolve, time) });
 }
 
 function randomColor () {
-	return `rgb(${random()}, ${random()}, ${random()})`
+	return `rgb(${random()}, ${random()}, ${random()})`;
 }
 
 function random () {
-	return 	Math.floor(((Math.random() * 4242 ) % 256))
+	return 	Math.floor(((Math.random() * 4242 ) % 256));
 }
+
+module.exports = HarlemShake;
